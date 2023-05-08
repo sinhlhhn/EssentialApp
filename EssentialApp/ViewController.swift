3c5584de0be748acb329dda5e38803db955bb522
Sinh Hoang <lehoangsinhhn99@gmail.com>
2023-05-08 09:43:45 +0700

    init

---
+++ EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by Developer on 08/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
