7e9de53c3328f2186f9c55a743cd63d9fb5c8ffb
Sinh Hoang <lehoangsinhhn99@gmail.com>
2023-08-04 16:31:23 +0700

    create FeedItem

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by sinhlh on 04/08/2023.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
