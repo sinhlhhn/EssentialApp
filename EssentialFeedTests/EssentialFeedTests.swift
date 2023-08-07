04a529249b5ccd9bd6f5d6e1a5f0bbd4956e183d
sinhlh <sinhlhhn@gmail.com>
2023-08-07 15:20:49 +0700

    inject url in concrete class

--- EssentialFeedTests/EssentialFeedTests.swift
+++ EssentialFeedTests/EssentialFeedTests.swift
@@ -9,13 +9,15 @@ import XCTest
 
 class RemoteFeedLoader {
     let client: HTTPClient
+    let url: URL
     
-    init(client: HTTPClient) {
+    init(client: HTTPClient, url: URL) {
         self.client = client
+        self.url = url
     }
     
     func load() {
-        client.get(from: URL(string: "https://a-url")!)
+        client.get(from: url)
     }
 }
 
@@ -40,19 +42,19 @@ final class EssentialFeedTests: XCTestCase {
     }
     
     func test_init_requestDataFromURL() {
-        
-        let (sut, client) = makeSUT()
+        let url = URL(string: "https://a-new-url")!
+        let (sut, client) = makeSUT(url: url)
         
         sut.load()
         
-        XCTAssertNotNil(client.requestedURL)
+        XCTAssertEqual(client.requestedURL, url)
     }
     
     //MARK: - Helpers
     
-    private func makeSUT(client: HTTPClient = HTTPClientSpy()) -> (RemoteFeedLoader, HTTPClientSpy) {
+    private func makeSUT(client: HTTPClient = HTTPClientSpy(), url: URL = URL(string: "https://a-url")!) -> (RemoteFeedLoader, HTTPClientSpy) {
         let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(client: client)
+        let sut = RemoteFeedLoader(client: client, url: url)
         return (sut, client)
     }
 }
