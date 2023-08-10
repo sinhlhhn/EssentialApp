d3860f6adecb2e2e17544375c7bec2c2e606eb14
sinhlh <sinhlhhn@gmail.com>
2023-08-10 17:20:20 +0700

    resumes data task on creation

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTest.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTest.swift
@@ -17,7 +17,7 @@ class URLSessionHTTPClient {
     func get(from url: URL) {
         session.dataTask(with: url) { data, response, error in
             
-        }
+        }.resume()
     }
 }
 
@@ -33,14 +33,44 @@ final class URLSessionHTTPClientTest: XCTestCase {
         XCTAssertEqual(session.receiveURLs, [url])
     }
     
+    func test_getFromURL_resumesDataTaskWithURL() {
+        let url = URL(string: "https://any-url")!
+        let session = URLSessionSpy()
+        let task = URLSessionTaskSpy()
+        session.stub(url: url, task: task)
+        let sut = URLSessionHTTPClient(session: session)
+        
+        sut.get(from: url)
+        
+        XCTAssertEqual(task.resumeCallCount, 1)
+    }
+    
     // MARK: - Helpers
     
     private class URLSessionSpy: URLSession {
         var receiveURLs: [URL] = []
+        private var stubs: [URL: URLSessionDataTask] = [:]
+        
+        func stub(url: URL, task: URLSessionDataTask) {
+            stubs[url] = task
+        }
         
         override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
             receiveURLs.append(url)
-            return URLSessionDataTask()
+            return stubs[url] ?? FakeURLSessionDataTask()
+        }
+        
+    }
+    
+    private class FakeURLSessionDataTask: URLSessionDataTask {
+        override func resume() { }
+    }
+    
+    private class URLSessionTaskSpy: URLSessionDataTask {
+        var resumeCallCount: Int = 0
+        
+        override func resume() {
+            resumeCallCount += 1
         }
     }
 }
