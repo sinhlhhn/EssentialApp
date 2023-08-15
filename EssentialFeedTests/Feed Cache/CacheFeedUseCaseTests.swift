5dfd68825055fdf20c69f7b7d13d8bf9eeedf97e
sinhlh <sinhlhhn@gmail.com>
2023-08-15 17:20:00 +0700

    save command request cache deletion

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -8,11 +8,23 @@
 import XCTest
 
 final class LocalFeedLoader {
+    private let store: FeedStore
     
+    init(store: FeedStore = FeedStore()) {
+        self.store = store
+    }
+    
+    func save() {
+        store.deleteCacheFeed()
+    }
 }
 
 final class FeedStore {
     var deleteCachedFeedCallCount = 0
+    
+    func deleteCacheFeed() {
+        deleteCachedFeedCallCount += 1
+    }
 }
 
 final class CacheFeedUseCaseTests: XCTestCase {
@@ -22,5 +34,14 @@ final class CacheFeedUseCaseTests: XCTestCase {
         _ = LocalFeedLoader()
         
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
+    
+    func test_save_requestCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        
+        sut.save()
+        
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
     }
 }
