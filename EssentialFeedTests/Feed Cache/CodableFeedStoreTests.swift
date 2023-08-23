a3f7f86b81bb802b0b631db6b941f21d1c678ac1
sinhlh <sinhlhhn@gmail.com>
2023-08-23 20:58:38 +0700

    Extract reusable `FeedStoreSpecs` helper methods into a shared scope so it can be used by other `FeedStoreSpecs` implementation tests

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -217,61 +217,6 @@ final class CodableFeedStoreTests: XCTestCase, FailableFeedStoreSpecs {
         return sut
     }
     
-    private func expect(_ sut: FeedStore, toRetrieveWithResult expectedResult: LoadCacheResult, file: StaticString = #filePath, line: UInt = #line) {
-        let exp = expectation(description: "wait for retrieval")
-        sut.retrieve { receivedResult in
-            switch (receivedResult, expectedResult) {
-                
-            case let (.find(receivedImages, receivedTimestamp), .find(expectedImages, expectedTimestamp)):
-                XCTAssertEqual(receivedImages, expectedImages)
-                XCTAssertEqual(receivedTimestamp, expectedTimestamp)
-                
-            case (.empty, .empty), (.failure, .failure): break
-                
-            default:
-                XCTFail("Expected retrieving \(expectedResult), got \(receivedResult) instead", file: file, line: line)
-            }
-            
-            exp.fulfill()
-        }
-        
-        wait(for: [exp], timeout: 1)
-    }
-    
-    @discardableResult
-    private func delete(from sut: FeedStore) -> Error? {
-        let exp = expectation(description: "wait for deletion")
-        var deletionError: Error?
-        sut.deleteCacheFeed { receivedError in
-            deletionError = receivedError
-            exp.fulfill()
-        }
-        
-        wait(for: [exp], timeout: 1)
-        
-        return deletionError
-    }
-    
-    @discardableResult
-    private func insert(_ feed: [LocalFeedImage], timestamp: Date, to sut: FeedStore) -> Error? {
-        let exp = expectation(description: "wait for insertion")
-        
-        var insertionError: Error?
-        sut.insert(feed, currentDate: timestamp) { receivedError in
-            insertionError = receivedError
-            exp.fulfill()
-        }
-        
-        wait(for: [exp], timeout: 1)
-        
-        return insertionError
-    }
-    
-    private func expect(_ sut: FeedStore, toRetrieveWithResultTwice expectedResult: LoadCacheResult) {
-        expect(sut, toRetrieveWithResult: expectedResult)
-        expect(sut, toRetrieveWithResult: expectedResult)
-    }
-    
     private func testSpecificStoreURL() -> URL {
         cacheDirectory().appendingPathComponent("\(type(of: self)).store")
     }
