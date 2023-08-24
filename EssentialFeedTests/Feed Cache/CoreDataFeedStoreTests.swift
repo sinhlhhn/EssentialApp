07131837c78c801c815f67c6562eab226e1693c7
sinhlh <sinhlhhn@gmail.com>
2023-08-24 15:10:21 +0700

    Add empty CoreData feed store specs

---
+++ EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -0,0 +1,83 @@
+//
+//  CoreDataFeedStoreTests.swift
+//  EssentialFeedTests
+//
+//  Created by Sam on 24/08/2023.
+//
+
+import XCTest
+
+final class CoreDataFeedStoreTests: XCTestCase, FailableFeedStoreSpecs {
+    
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectOnEmptyCache() {
+        
+    }
+    
+    func test_retrieve_deliversFoundValueOnNonEmptyCache() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectOnNonEmptyCache() {
+        
+    }
+    
+    func test_insert_deliversNoErrorOnEmptyCache() {
+        
+    }
+    
+    func test_insert_deliversNoErrorOnNonEmptyCache() {
+        
+    }
+    
+    func test_insert_overridesPreviouslyInsertedCache() {
+        
+    }
+    
+    func test_delete_deliversNoErrorOnEmptyCache() {
+        
+    }
+    
+    func test_delete_hasNoSideEffectOnEmptyCache() {
+        
+    }
+    
+    func test_delete_deliversNoErrorOnNonEmptyCache() {
+        
+    }
+    
+    func test_delete_emptiesPreviouslyInsertedCached() {
+        
+    }
+    
+    func test_storeSideEffect_runSerially() {
+        
+    }
+    
+    func test_retrieve_deliverFailureOnRetrievalError() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectOnRetrievalError() {
+        
+    }
+    
+    func test_insert_deliversFailureOnInsertionError() {
+        
+    }
+    
+    func test_insert_hasNoSideEffectOnInsertionError() {
+        
+    }
+    
+    func test_delete_deliverErrorOnDeletionError() {
+        
+    }
+    
+    func test_delete_hasNoSideEffectOnDeletionError() {
+        
+    }
+}
