b3d5f9d70141ef587620da2f9103cae6b08a0387
sinhlh <sinhlhhn@gmail.com>
2023-09-04 23:50:11 +0700

    move `FeedViewController` to production target

---
+++ EssentialFeediOS/FeedViewController.swift
@@ -0,0 +1,34 @@
+//
+//  FeedViewController.swift
+//  EssentialFeediOS
+//
+//  Created by Sam on 04/09/2023.
+//
+
+import UIKit
+import EssentialFeed
+
+public final class FeedViewController: UITableViewController {
+    var loader: FeedLoader?
+    
+    public convenience init(loader: FeedLoader) {
+        self.init()
+        self.loader = loader
+    }
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
+        
+        load()
+    }
+    
+    @objc private func load() {
+        refreshControl?.beginRefreshing()
+        loader?.load { [weak self] _ in
+            self?.refreshControl?.endRefreshing()
+        }
+    }
+}
