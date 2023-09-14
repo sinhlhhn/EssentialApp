fd0fecedc0138f464614b5a793954bfbd923b50b
sinhlh <sinhlhhn@gmail.com>
2023-09-14 16:59:55 +0700

    `FeedPresenter` display no error on `didStartLoading`

--- EssentialFeedTests/FeedPresenter/FeedPresenterTests.swift
+++ EssentialFeedTests/FeedPresenter/FeedPresenterTests.swift
@@ -8,8 +8,29 @@
 import Foundation
 import XCTest
 
-final class FeedPresenter {
+struct FeedErrorViewModel {
+    let message: String?
     
+    static var noError: FeedErrorViewModel {
+        return FeedErrorViewModel(message: nil)
+    }
+
+}
+
+protocol FeedErrorView {
+    func display(_ viewModel: FeedErrorViewModel)
+}
+
+final class FeedPresenter {
+    private let feedErrorView: FeedErrorView
+    
+    init(feedErrorView: FeedErrorView) {
+        self.feedErrorView = feedErrorView
+    }
+    
+    func didStartLoading() {
+        feedErrorView.display(.noError)
+    }
 }
 
 final class FeedPresenterTests: XCTestCase {
@@ -19,17 +40,34 @@ final class FeedPresenterTests: XCTestCase {
         XCTAssertTrue(view.messages.isEmpty)
     }
     
+    func test_didStartLoading_displayNoErrorMessage() {
+        let (sut, view) = makeSUT()
+        
+        sut.didStartLoading()
+        
+        XCTAssertEqual(view.messages, [.display(errorMessage: .none)])
+    }
+    
     //MARK: -Helpers
     
     private func makeSUT() -> (FeedPresenter, ViewSpy) {
-        let sut = FeedPresenter()
         let view = ViewSpy()
+        let sut = FeedPresenter(feedErrorView: view)
         
         return (sut, view)
     }
     
-    private class ViewSpy {
-        private(set) var messages: [Any] = []
+    private class ViewSpy: FeedErrorView {
+        
+        enum Message: Equatable {
+            case display(errorMessage: String?)
+        }
+        
+        private(set) var messages: [Message] = []
+        
+        func display(_ viewModel: FeedErrorViewModel) {
+            messages.append(.display(errorMessage: viewModel.message))
+        }
     }
 }
 
