32e286ccbdcf8767b0983e50a39f6296600627ff
sinhlh <sinhlhhn@gmail.com>
2023-09-14 17:52:39 +0700

    replace FeedPresenter from iOS module with the `FeedPresenter` from the cross-platform Essential module

--- EssentialFeediOS/FeedUI/Composer/FeedViewAdapter.swift
+++ EssentialFeediOS/FeedUI/Composer/FeedViewAdapter.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import UIKit
+import EssentialFeed
 
 class FeedViewAdapter: FeedView {
     private weak var controller: FeedViewController?
--- EssentialFeediOSTests/FeedUI/Controllers/FeedLocalizationTests.swift
+++ EssentialFeediOSTests/FeedUI/Controllers/FeedLocalizationTests.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 import XCTest
-@testable import EssentialFeediOS
+import EssentialFeed
 
 final class FeedLocalizationTests: XCTestCase {
     
