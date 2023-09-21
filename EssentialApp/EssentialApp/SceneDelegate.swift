bd4f72292ecae0cf1b70ad7765ae34d7474e3df8
sinhlh <sinhlhhn@gmail.com>
2023-09-21 15:00:19 +0700

    create Essential workplace combining EssentialApp and EssentialFeed projects

---
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  SceneDelegate.swift
+//  EssentialApp
+//
+//  Created by Sam on 21/09/2023.
+//
+
+import UIKit
+import EssentialFeed
+import EssentialFeediOS
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        let url = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
+        let session = URLSession(configuration: .ephemeral)
+        let client = URLSessionHTTPClient(session: session)
+        let imageLoader = RemoteFeedImageDataLoader(client: client)
+        let feedLoader = RemoteFeedLoader(client: client, url: url)
+        let feedViewController = FeedUIComposer.feedComposedWith(loader: feedLoader, imageLoader: imageLoader)
+        
+        window?.rootViewController = feedViewController
+    }
+}
+
