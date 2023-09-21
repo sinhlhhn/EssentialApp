f1477f91930005f617c6faddafe3d709840ac083
sinhlh <sinhlhhn@gmail.com>
2023-09-21 16:53:12 +0700

    `FeedLoaderWithCallbackComposite.load` delivers fallback feed on primary loader failure

--- EssentialApp/EssentialAppTests/FeedLoaderWithCallbackCompositeTests.swift
+++ EssentialApp/EssentialAppTests/FeedLoaderWithCallbackCompositeTests.swift
@@ -10,13 +10,22 @@ import EssentialFeed
 
 final class FeedLoaderWithCallbackComposite: FeedLoader {
     private let primaryLoader: FeedLoader
+    private let callbackLoader: FeedLoader
     
     init(primaryLoader: FeedLoader, callbackLoader: FeedLoader) {
         self.primaryLoader = primaryLoader
+        self.callbackLoader = callbackLoader
     }
     
     func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        primaryLoader.load(completion: completion)
+        primaryLoader.load { [weak self] result in
+            switch result {
+            case .success:
+                completion(result)
+            case .failure:
+                self?.callbackLoader.load(completion: completion)
+            }
+        }
     }
 }
 
@@ -32,6 +41,24 @@ final class FeedLoaderWithCallbackCompositeTests: XCTestCase {
             switch result {
             case let .success(receivedImage):
                 XCTAssertEqual(receivedImage, primaryImage)
+            case let .failure(error):
+                XCTFail("Expected success got \(error) instead")
+            }
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1)
+    }
+    
+    func test_loadFeed_deliversFallbackOnPrimaryLoaderFailure() {
+        let callbackImage = uniqueImage()
+        let sut = makeSUT(primaryResult: .failure(anyNSError()), callbackResult: .success(callbackImage))
+        
+        let exp = expectation(description: "wait for load")
+        sut.load { result in
+            switch result {
+            case let .success(receivedImage):
+                XCTAssertEqual(receivedImage, callbackImage)
             case let .failure(error):
                 XCTFail("Expected success got \(error) instead")
             }
@@ -62,6 +89,10 @@ final class FeedLoaderWithCallbackCompositeTests: XCTestCase {
     
     private func uniqueImage() -> [FeedImage] {
         return [FeedImage(id: UUID(), description: nil, location: nil, url: URL(string: "https://any-url")!)]
+    }
+    
+    private func anyNSError() -> NSError {
+        return NSError(domain: "e", code: 1)
     }
     
     private class FeedLoaderStub: FeedLoader {
