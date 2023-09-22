9b8d4250fb274f082236c9fea392f30d5972777e
sinhlh <sinhlhhn@gmail.com>
2023-09-22 16:32:17 +0700

    `FeedLoaderCacheDecorator.load` delivers decoratee loader result (either success or failure)

---
+++ EssentialApp/EssentialAppTests/FeedLoaderCacheDecoratorTests.swift
@@ -0,0 +1,76 @@
+//
+//  FeedLoaderCacheDecoratorTests.swift
+//  EssentialAppTests
+//
+//  Created by Sam on 22/09/2023.
+//
+
+import XCTest
+import EssentialFeed
+
+final class FeedLoaderCacheDecorator: FeedLoader {
+    private let decoratee: FeedLoader
+    
+    init(decoratee: FeedLoader) {
+        self.decoratee = decoratee
+    }
+    
+    func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        decoratee.load(completion: completion)
+    }
+}
+
+final class FeedLoaderCacheDecoratorTests: XCTestCase {
+    func test_load_deliversFeedOnLoaderSuccess() {
+        let feed = uniqueImage()
+        let loader = FeedLoaderSpy(result: .success(feed))
+        let sut = FeedLoaderCacheDecorator(decoratee: loader)
+        
+        expect(sut, completeWithResult: .success(feed))
+    }
+    
+    func test_load_deliversFeedOnLoaderFailure() {
+        let error = anyNSError()
+        let loader = FeedLoaderSpy(result: .failure(error))
+        let sut = FeedLoaderCacheDecorator(decoratee: loader)
+        
+        expect(sut, completeWithResult: .failure(error))
+    }
+    
+    //MARK: -Helpers
+    
+    private func uniqueImage() -> [FeedImage] {
+        return [FeedImage(id: UUID(), description: nil, location: nil, url: URL(string: "https://any-url")!)]
+    }
+    
+    private func expect(_ sut: FeedLoaderCacheDecorator, completeWithResult expectedResult: FeedLoader.Result, file: StaticString = #filePath, line: UInt = #line) {
+        
+        let exp = expectation(description: "wait for load")
+        sut.load { result in
+            switch (result, expectedResult) {
+            case let (.success(receivedData), .success(expectedData)):
+                XCTAssertEqual(receivedData, expectedData, file: file, line: line)
+            case let (.failure(receivedError as NSError), .failure(expectedError as NSError)):
+                XCTAssertEqual(receivedError.code, expectedError.code, file: file, line: line)
+                XCTAssertEqual(receivedError.domain, expectedError.domain, file: file, line: line)
+            default:
+                XCTFail("Expected \(expectedResult) got \(result) instead", file: file, line: line)
+            }
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1)
+    }
+    
+    private class FeedLoaderSpy: FeedLoader {
+        let result: FeedLoader.Result
+        
+        init(result: FeedLoader.Result) {
+            self.result = result
+        }
+        
+        func load(completion: @escaping (FeedLoader.Result) -> Void) {
+            completion(result)
+        }
+    }
+}
