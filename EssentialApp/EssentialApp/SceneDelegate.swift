009b45efaa82cbddba8aef63525dfca79e488b55
sinhlh <sinhlhhn@gmail.com>
2023-09-23 10:47:34 +0700

    remove code to use test drive technique

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -17,15 +17,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
         guard let _ = (scene as? UIWindowScene) else { return }
-        
-        let url = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
-        let session = URLSession(configuration: .ephemeral)
-        let client = URLSessionHTTPClient(session: session)
-        let imageLoader = RemoteFeedImageDataLoader(client: client)
-        let feedLoader = RemoteFeedLoader(client: client, url: url)
-        let feedViewController = FeedUIComposer.feedComposedWith(loader: feedLoader, imageLoader: imageLoader)
-        
-        window?.rootViewController = feedViewController
     }
 }
 
